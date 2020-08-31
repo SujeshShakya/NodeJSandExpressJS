8120b467272dce123c00001c9e81805caeb65c44
Sujesh <sujesh@uba-solutions.com>
2020-08-31 17:33:38 +1200

    Add files via upload

---
+++ db.js
@@ -0,0 +1,29 @@
+
+var crypto = require('crypto');
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(
+	'test_app',
+	'root',
+	'root',
+	{
+    dialect: 'mysql',
+    logging: console.log,
+		define: {
+			timestamps: false
+		}
+	}
+);
+
+const db= {};
+
+db.Sequelize = Sequelize;
+db.Sequelize = Sequelize;
+
+// db.users = require('./models/users')(sequelize, Sequelize);
+
+// db.users.belongsTo(db.user_types,{
+// foreignKey: 'user_type_id'
+// });
+
+module.exports = db;
